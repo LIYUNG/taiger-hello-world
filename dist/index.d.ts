803b8f9e90404cba105f8c366963ac7baf30dd85
Leo <liyung.chen@tum.de>
2024-09-30 22:40:01 +0200

    hell world init commit

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export declare function helloWorld(): string;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+export function helloWorld() {
+    return 'Hello, world!';
+}
+//# sourceMappingURL=index.js.map
