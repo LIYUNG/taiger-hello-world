41347da4e98d57eb48944f7b7536be36be06a3a1
Leo <liyung.chen@tum.de>
2024-11-23 15:54:55 +0100

    fix: update

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,3 +1,5 @@
-export function helloWorld(): string {
-  return 'Hello, world!';
+export function helloWorldFunc(): string {
+  return 'Hello, world function!';
 }
+
+export const helloWorldSring = 'Hello, world String!';
